9a27ef2ababbd5677a6d8de31f358da6d22a082b
ecfarrer <emilycfarrer@gmail.com>
2017-09-01 17:30:10 -0500

    added code for niwot data in the hmsc file, added comments to the biogeochemistry file

--- Biogeochemistry.R
+++ Biogeochemistry.R
@@ -1,6 +1,7 @@
 
 biogeo<-read.csv("/Users/farrer/Dropbox/EmilyComputerBackup/Documents/Niwot_King/Figures&Stats/kingdata/Biogeochemistry/CN_enzymes_pH_moisture_whc_Niwot2015t.csv")
 head(biogeo)
+#note on dataset - I updated this because for the data from microbial biomass (gravimetric moisture, IN, DOC, etc), sample 33 and 34 were mixed up. origianlly sample 33 was missing from the first dataset dorota gave me (CN_enzymes_pH_moisture_whc_Niwot2015.xlsx), and the nubmers fom 34 were really the values for 33. Then I got the All Data file from Dorota and this cleared it up b/c it had both samples 33 adn 34 in it, I also double checked with the Biogeochemistry_Niwot_2015.xlsx file where the calculations were done.
 
 #dataframe that has lomehi in it, datBacr3fotu
 
@@ -8,6 +9,7 @@ biogeo2<-merge(biogeo,datBacr3fotu[,1:31],by="X.SampleID")
 
 dim(biogeo2)
 
+#I replaced all negative numbers with 0, not sure if I should do this, since they are relative, but it doesn't really make sense to have negative microbial biomass or inorganic N
 biogeo2[biogeo2<0]<-0
 
 biogeom<-biogeo2%>%
--- HMSC.R
+++ HMSC.R
@@ -1,11 +1,21 @@
 #HMSC
 
+# To install HMSC: 
+#   install Xcode
+#   make sure command line tools are installed by typing "make" in the terminal
+#   install gfortran 6.3 for sierra
+#   follow instructions on http://thecoatlessprofessor.com/programming/rcpp-rcpparmadillo-and-os-x-mavericks-lgfortran-and-lquadmath-error/ for doing the following in terminal:
+#   mkdir ~/.R
+#   cat << EOF >> ~/.R/Makevars
+#   FLIBS=-L/usr/local/gfortran/lib/gcc/x86_64-apple-darwin16/6.3.0 -L/usr/local/gfortran/lib -lgfortran -lquadmath -lm
+#   EOF
+#   install dependencies and HMSC follwoing GitHub instructions: https://github.com/guiblanchet/HMSC
+
 library(HMSC)
 
 data("simulEx1")
 data("simulParamEx1") #true parameters
-model <- hmsc(simulEx1, family = "probit", niter = 10000, nburn = 1000,
-              thin = 10)
+model <- hmsc(simulEx1, family = "probit", niter = 10000, nburn = 1000, thin = 10)
 
 #save results outside of the work env, but it just saves it as an R workspace
 save(model, file = "/Users/farrer/Dropbox/EmilyComputerBackup/Documents/Niwot_King/Figures&Stats/kingdata/HMSC/SimulatedExample1/model.RData")
@@ -62,8 +72,7 @@ corMat <- corRandomEff(model,cor=FALSE)
   ### Sampling units level
 #----------------------------------------------------------------------------
 ### Isolate the values of interest
-ltri <- lower.tri(apply(corMat[, , , 1], 1:2, quantile, probs = 0.025),
-                  diag=TRUE)
+ltri <- lower.tri(apply(corMat[, , , 1], 1:2, quantile, probs = 0.025),diag=TRUE)
 ### True values
 truth <- as.vector(tcrossprod(simulParamEx1$param$paramLatent[[1]])[ltri])
 ### Average
@@ -102,8 +111,7 @@ beanplot(mixingDF, las = 2)
 library(corrplot)
 corMat <- corRandomEff(model, cor = TRUE)
 averageCor <- apply(corMat[, , , 1], 1:2, mean)
-corrplot(averageCor, method = "color",
-         col = colorRampPalette(c("blue", "white", "red"))(200))
+corrplot(averageCor, method = "color",col = colorRampPalette(c("blue", "white", "red"))(200))
 ### Draw chord diagram
 library(circlize)
 corMat <- corRandomEff(model, cor = TRUE)
@@ -119,8 +127,7 @@ chordDiagram(averageCor, symmetric = TRUE,
 ### Plot level
 #----------------------------------------------------------------------------
 ### Isolate the values of interest
-ltri <- lower.tri(apply(corMat[, , , 2], 1:2, quantile, probs=0.025),
-                  diag=TRUE)
+ltri <- lower.tri(apply(corMat[, , , 2], 1:2, quantile, probs=0.025),diag=TRUE)
 ### True values
 truth <- as.vector(tcrossprod(simulParamEx1$param$paramLatent[[2]])[ltri])
 ### Average
@@ -212,8 +219,170 @@ predVal <- predict(model, dataVal)
 
 
 #for real data
-formdata <- as.HMSCdata(Y = Y, X = X, Random = Pi, interceptX = T,
-                        scaleX=T)
-formprior <- as.HMSCprior(formdata)
-formparam <- as.HMSCparam(formdata, formprior)
 
+comm.dataALL
+
+hmscY<-comm.dataALL[,32:6704]
+hmscY<-comm.dataALL[,32:1000] #for practice
+rownames(hmscY)<-comm.dataALL$X.SampleID
+
+biogeo3<-biogeo2[order(biogeo2$Sample_name),]#now they are in the same order
+hmscX<-data.frame(inter=rep(1,90),pH=biogeo3$pH.x,lomehi=biogeo3$lomehi) #there are NAs in the pH data and possibly more, moisture=biogeo3$moisture.x,TN=biogeo3$TN,TC=biogeo3$TC,
+rownames(hmscX)<-biogeo3$X.SampleID
+
+#take out any rows with NAs
+ind<-which(!is.na(rowSums(hmscX[,1:2])))
+hmscXb<-hmscX[ind,]
+hmscYb<-hmscY[ind,]
+
+#readjust the lo me hi categories based on NAs and # samples in each category
+#get plant cover data to use as "light"
+
+#select lo
+ind<-which(hmscXb$lomehi=="lo")
+hmscXc<-hmscXb[ind,]
+hmscYc<-hmscYb[ind,]
+
+#select species with greater than 10 (11 or more) and remove lo me hi (since you can't have text in a matrix or the whole matrix becomes character)
+ind<-which(colSums(hmscYc>0)>10)
+hmscYd<-hmscYc[,ind]
+hmscXd<-hmscXc[,1:2]
+
+#testing a really strong correlation with pH
+#hmscYd[,2]<-rnorm(29,mean=2*formdata$X[,2]+1,sd=.02)
+
+#select only species 2 and 3
+#hmscYd<-cbind(hmscYd[,3],hmscYd[,3])
+
+#check if the values are too low that some tolerance is messing up the CI estimates, yes important to scale y
+hmscYe<-scale(hmscYd)
+
+#make them matrices
+hmscXe<-as.matrix(hmscXd)
+hmscYe<-as.matrix(hmscYe)
+
+#the y data are not normal (the only options I have are normal, binary, poisson, overdispersed poisson), so I could do a sqrt transformation on Y (log(0) is -Inf)
+#hmscYf<-sqrt(hmscYe)
+
+#make the random (residual matrix)
+pimat<-data.frame(plot=1:dim(hmscYe)[1])
+pimat$plot<-as.factor(pimat$plot)
+rownames(pimat)<-rownames(hmscYe)
+
+formdata <- as.HMSCdata(Y = hmscYe, X = hmscXe, Random=pimat,interceptX = F, scaleX=T)
+#formdata <- as.HMSCdata(Y = hmscYe, X = hmscXe,interceptX = F, scaleX=T)
+formprior <- as.HMSCprior(formdata,family="gaussian") #not necessary, this just generates flat priors
+formparam <- as.HMSCparam(formdata, formprior) #not necessary, this just generates random staring parameters
+
+model <- hmsc(formdata, family = "gaussian", niter = 10000, nburn = 1000, thin = 10)
+
+mixing <- as.mcmc(model,parameters = "paramX")
+temp<-as.vector(mixing[1:900,1])
+hist(temp)
+plot(temp,type="l")
+
+### Convert the mixing object to a matrix
+mixingDF <- as.data.frame(mixing)
+boxplot(mixingDF[,2], las = 2)
+
+#CI
+average <- apply(model$results$estimation$paramX, 1:2, mean)
+### 95% confidence intervals
+CI.025 <- apply(model$results$estimation$paramX, 1:2, quantile, probs = 0.025)
+CI.975 <- apply(model$results$estimation$paramX, 1:2, quantile, probs = 0.975)
+CI <- cbind(as.vector(CI.025), as.vector(CI.975))
+
+plot(0, 0, xlim = c(1, nrow(CI)), ylim = range(CI), type = "n", xlab = "", ylab = "", main="paramX")
+abline(h = 0,col = "grey")
+arrows(x0 = 1:nrow(CI), x1 = 1:nrow(CI), y0 = CI[, 1], y1 = CI[, 2], code = 3, angle = 90, length = 0.05)
+points(1:nrow(CI), average, pch = 15, cex = 1.5)
+
+
+### Summary table
+paramXCITable <- cbind(unlist(as.data.frame(average)),
+                       unlist(as.data.frame(CI.025)),
+                       unlist(as.data.frame(CI.975)))
+colnames(paramXCITable) <- c("paramX", "lowerCI", "upperCI")
+rownames(paramXCITable) <- paste(rep(colnames(average),
+                                     each = nrow(average)), "_",
+                                 rep(rownames(average),
+                                     ncol(average)), sep="")
+
+#variance partitioning
+variationPart <- variPart(model,c("climate","habitat"))
+barplot(t(variationPart), legend.text=colnames(variationPart),
+        args.legend=list(y=1.1, x=nrow(variationPart)/2, xjust=0.5, horiz=T))
+
+#R2
+Ymean <- apply(model$data$Y,2,mean)
+R2 <- Rsquared(model, averageSp=FALSE)
+plot(Ymean,R2,pch=19)
+
+#species correlations
+library(corrplot)
+
+corMat <- corRandomEff(model,cor=FALSE) #cor=F gives the covariance
+ltri <- lower.tri(apply(corMat[, , , 1], 1:2, quantile, probs = 0.025),diag=TRUE)
+### Average
+average <- as.vector(apply(corMat[, , , 1], 1:2, mean)[ltri])
+### 95% confidence intervals
+corMat.025 <- as.vector(apply(corMat[, , , 1], 1:2, quantile,probs = 0.025)[ltri])
+corMat.975 <- as.vector(apply(corMat[, , , 1], 1:2, quantile,probs=0.975)[ltri])
+CI <- cbind(corMat.025, corMat.975)
+plot(0, 0, xlim = c(1, nrow(CI)), ylim = range(CI), type = "n", xlab = "", main = "cov(paramLatent[[1, 1]])")
+abline(h = 0, col = "grey")
+arrows(x0 = 1:nrow(CI), x1 = 1:nrow(CI), y0 = CI[, 1], y1 = CI[, 2], code = 3, angle = 90, length = 0.05)
+points(1:nrow(CI), average, pch = 15,cex = 1.5)
+
+corMat <- corRandomEff(model, cor = TRUE)
+averageCor <- apply(corMat[, , , 1], 1:2, mean)
+corrplot(averageCor, method = "color", col = colorRampPalette(c("blue", "white", "red"))(200))
+
+(a) P from CI for a difference
+If the upper and lower limits of a 95% CI are u and l respectively:
+1 calculate the standard error: SE = (u − l)/(2*1.96)
+2 calculate the test statistic: z = Est/SE
+3 calculate the P value2: P = exp(−0.717×z − 0.416×z2).
+
+library(circlize)
+corMat <- corRandomEff(model, cor = TRUE)
+averageCor <- apply(corMat[, , , 1], 1:2, mean)
+colMat <- matrix(NA, nrow = nrow(averageCor), ncol = ncol(averageCor))
+colMat[which(averageCor > 0.6, arr.ind = TRUE)] <- "red"
+colMat[which(averageCor < -0.6, arr.ind = TRUE)] <- "blue"
+chordDiagram(averageCor, symmetric = TRUE,
+             annotationTrack = c("name", "grid"),
+             grid.col = "grey",col=colMat)
+
+
+
+#checking, for species 2 and 3 intercept and slope should be significant
+plot(formdata$X[,2],formdata$Y[,2])
+abline(a=-2.571260,b=0.5421437)
+abline(a=-7.1447,b=1.4906,col=2)
+summary(lm(formdata$Y[,1]~formdata$X[,2]))
+summary(lm(formdata$Y[,4]~formdata$X[,2]))
+
+
+#trying just calculating intercept on simulated data, everything checks out, confidence intervals are correct and standard error in lm is the same as the calucated SE and you can calculate the CI from the standard error in lm()
+hmscYd<-as.matrix(data.frame(y1=rnorm(1000,mean=1,sd=.2),y2=rnorm(1000,mean=1,sd=.2)))
+hmscXd<-as.matrix(rep(1,1000))
+
+formdata <- as.HMSCdata(Y = hmscYd, X = hmscXd, interceptX = F, scaleX=T)
+model <- hmsc(formdata, family = "gaussian", niter = 10000, nburn = 1000, thin = 10)
+0.99505+1.96*.2/sqrt(1000)
+summary(lm(hmscYd[,1]~1))
+.2/sqrt(1000)
+sd(hmscYd)/sqrt(1000)
+0.995050-0.006077*1.96
+0.995050+0.006077*1.96
+
+#trying to calculate likelihood
+-sum(dnorm(formdata$Y[,2],mean=-100+1.4906*formdata$X[,2],sd=1,log=T)) 
+-sum(dnorm(formdata$Y[,2],mean=-7.1447+1.4906*formdata$X[,2],sd=1,log=T)) #I don't know the sd, but I think it doesn't matter if I'm only looking at relative differences in loglik
+-sum(dnorm(formdata$Y[,2],mean=-2.5712604+0.5421437*formdata$X[,2],sd=1,log=T))
+#in terms of likelihood, the lm() model estimates are better
+
+#upshot
+#mcmc is very sensitive to the range in the y variables, if the numbers are too low, the CI will be huge b/c somehow it takes huge jumps in the mcmc - solution: scale the Y
+#mcmc is also sensitive to the absolute value of the x variables (but I did this on my original data trial so this was not the problem then). even if it ranges from 6-8 the mcmc gives non-optimal results compared if the range is -1 to 1 
