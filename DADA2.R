b06476e5f5c125078f1424883d685a124775085a
farrer <ecfarrer@berkeley.edu>
2018-01-05 13:55:21 -0600

    code fo DADA2 and QIIME2

---
+++ DADA2.R
@@ -0,0 +1,13 @@
+#DADA2
+
+#installation instructions: https://benjjneb.github.io/dada2/dada-installation.html
+# try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("BiocUpgrade") #upgrade to version 3.6
+biocLite("dada2")
+
+library(dada2); packageVersion("dada2") #should be version 1.6, yes
+
+
+#Following tutorial
+
